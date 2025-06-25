caf696ebf7bacc6f1865bf68c6a12c9e14a3334c
lucasmarques2907 <lucasredra@gmail.com>
2025-06-25 15:08:23 -0300

    refactor: changing 'print' to 'debugPrint'

--- lib/example/example.dart
+++ lib/example/example.dart
@@ -1,12 +1,15 @@
 import 'package:cep_fetcher/cep_fetcher.dart';
+import 'package:flutter/foundation.dart';
 
 void main() async {
   final fetcher = CepFetcher();
-  final result = await fetcher.fetchCepData('99999999');
+  final result = await fetcher.fetchCepData('01001000');
 
   if (result == null) {
-    print('CEP não encontrado');
+    debugPrint('CEP não encontrado');
   } else {
-    print('Logradouro: ${result.address}, ${result.city} - ${result.state}');
+    debugPrint(
+      'Logradouro: ${result.address}, ${result.city} - ${result.state}',
+    );
   }
 }
